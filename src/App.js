f85ba0cefec265022c67f3b25a909135e178df5d
ValaBindiya <bindiyavala6@gmail.com>
2024-02-29 15:21:59 +0530

    29/2/2024_Home-work

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - components function
+          Learn React - components function - Home work
         </a>
       </header>
     </div>
