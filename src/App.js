4e8f874a83184e99de766c40f865866d0e087ccf
ValaBindiya <bindiyavala6@gmail.com>
2024-02-29 14:33:28 +0530

    29/2/2024_Directary_structure

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - Directary structure
         </a>
       </header>
     </div>
