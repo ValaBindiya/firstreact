f28f087c2c609efa85d97b0b15a567f73e113645
ValaBindiya <bindiyavala6@gmail.com>
2024-02-29 15:07:50 +0530

    4/4/2024_component function

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - props
+          Learn React - components function
         </a>
       </header>
     </div>
