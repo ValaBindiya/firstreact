7a4b52c3e2fd8b4137965c636119831e84db72a6
ValaBindiya <bindiyavala6@gmail.com>
2024-03-01 18:32:44 +0530

    1/3/2024_Home_work

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
 class Country extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            name:'Surat',
+            population:'89.30 Lakhs'
+        }
+    }
+
+    changeCity = () => {
+        this.setState ({
+            name:'Ahemdabad',
+            population:'8.85 Lakhs'
+        })
+
+        // this.setState ({
+        //     name:'Vadodra',
+        //     population:'21.8 Lakhs'
+        // })
+    }
+    
     render() {
         return (
 
-            <div>Country</div>
+            <>
+                <h1>Class base stat components</h1>
+                <h1>Country name is : {this.state.name}</h1>
+                <h1>Time Zone is : {this.state.population}</h1>
+
+                <button onClick={this.changeCity}>Change city</button>
+            </>
         );
     }
 }
